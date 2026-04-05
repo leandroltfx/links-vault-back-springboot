f35ff489c1042fbc12cc80ee4aa2af35c0dcecec
leandroltfx <leandroltfbr@hotmail.com>
2026-04-05 09:09:02 -0300

    Inicia o projeto

---
+++ src/main/java/br/com/links_vault_back_springboot/LinksVaultBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.links_vault_back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksVaultBackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksVaultBackSpringbootApplication.class, args);
+	}
+
+}
